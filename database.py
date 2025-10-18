722fa70ec3907cf84a9227fc4f0b620f3790dead
kajanthann <arulkajanthan904@gmail.com>
2025-10-18 20:41:24 +0530

    mongoDB online

--- database.py
+++ database.py
@@ -1,11 +1,13 @@
-from motor.motor_asyncio import AsyncIOMotorClient 
 import os
+from motor.motor_asyncio import AsyncIOMotorClient
 from dotenv import load_dotenv
 
+# Load .env locally
 load_dotenv()
 
-MONGO_URL = os.getenv("MONGO_URL")
-DB_NAME = os.getenv("DB_NAME")
+MONGO_URL = os.environ.get("MONGO_URL")
+DB_NAME = os.environ.get("DB_NAME", "fastAPI_ToDo")
 
+# MongoDB client
 client = AsyncIOMotorClient(MONGO_URL)
 db = client[DB_NAME]
